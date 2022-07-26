2f0fdf97442fedd2825c9898ffe6b3cc882c5d6c
Junhyung <amailemon98@naver.com>
2022-07-26 12:13:47 +0900

    포인터였던 최상단 코드를 참조자를 이용해 변형시킨 코드

--- RePracticeing_C++_Again_For_Foundation/AboutReference.cpp
+++ RePracticeing_C++_Again_For_Foundation/AboutReference.cpp
@@ -26,3 +26,17 @@ int main() {
 
 	return 0;
 }*/
+#include <iostream>
+
+int change_val(int& p) {
+	p = 3;
+
+	return 0;
+}
+int main() {
+	int number = 5;
+
+	std::cout << number << std::endl;
+	change_val(number);
+	std::cout << number << std::endl;
+}
