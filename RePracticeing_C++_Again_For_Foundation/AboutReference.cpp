ca8296ad46bdf567094f16d780a355dc9ff4095f
Junhyung <amailemon98@naver.com>
2022-07-26 12:47:29 +0900

    참조자 이해용 (x=y=z)

--- RePracticeing_C++_Again_For_Foundation/AboutReference.cpp
+++ RePracticeing_C++_Again_For_Foundation/AboutReference.cpp
@@ -25,7 +25,7 @@ int main() {
 	std::cout << "another_a : " << another_a << std::endl;
 
 	return 0;
-}*/
+}*//*
 #include <iostream>
 
 int change_val(int& p) {
@@ -39,4 +39,23 @@ int main() {
 	std::cout << number << std::endl;
 	change_val(number);
 	std::cout << number << std::endl;
-}
+}*/
+
+/*
+//참조자 이해하기
+#include <iostream>
+
+int main() {
+	int x;
+	int& y = x;
+	int& z = y;
+
+	x = 1;
+	std::cout << "x : " << x << " y : " << y << " z : " << z << std::endl;
+
+	y = 2;
+	std::cout << "x : " << x << " y : " << y << " z : " << z << std::endl;
+
+	z = 3;
+	std::cout << "x : " << x << " y : " << y << " z : " << z << std::endl;
+}*/
