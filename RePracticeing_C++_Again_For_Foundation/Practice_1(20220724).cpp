79a62f251ea0a12f402a6de3e279214673644952
Junhyung <amailemon98@naver.com>
2022-07-26 11:20:33 +0900

    참조가 어떻게 되는지, 참조자를 생성하는 법 c++

--- RePracticeing_C++_Again_For_Foundation/Practice_1(20220724).cpp
+++ RePracticeing_C++_Again_For_Foundation/Practice_1(20220724).cpp
@@ -1,6 +1,18 @@
-#include <iostream> 
+/*#include <iostream> 
 
 int main() {
 	std::cout << "Hello, World!!" << std::endl;
 	return 0;
+}*/
+/*
+#include "header1.h"
+#include "header2.h"
+#include <iostream>
+
+namespace header1 {
+	int func() {
+		foo();
+		header2::foo();
+	}
 }
+*/
